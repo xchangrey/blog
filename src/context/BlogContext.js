bf918fc073c10e04e7e7c2ff7f0b2969320aba30
Christian Geroy <christian.geroy@weareadaptive.com>
2025-01-02 17:32:13 +0800

    feat(blog-rn): add context to manage blog posts
    - Add BlogContext.js to manage blog posts
    - Add IndexScreen.js to display blog posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BlogContext = React.createContext();
 
-export const BlogProvider = ({children}) => {
-    return <BlogContext.Provider value={5}>
+export const BlogProvider = ({ children }) => {
+    const [blogPosts, setBlogPosts] = useState([])
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}`}])
+    }
+
+    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
         {children}
     </BlogContext.Provider>
 };
